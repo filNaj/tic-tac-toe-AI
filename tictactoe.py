263d56750412cd5df0002950783b6bdd216286eb
Filip <filipnajjar@yahoo.com>
2023-07-01 11:40:47 -0400

    finished insertLetter()

--- tictactoe.py
+++ tictactoe.py
@@ -2,15 +2,28 @@ import tkinter as tk
 import random
 
 
-def insertBoard(letter, pos):
-    pass
+def insertLetter(row, column):
+    global player
+    if player == 'X':
+        if board[row][column]['text'] == '' and not isWinner():
+            board[row][column]['text'] = 'X'
+
+            if not isWinner():
+                player = 'O'
+                label.config(text='O Turn ')
+
+            elif isWinner():
+                label.config(text='You Won!')
+
+            elif isWinner() == 'Tie':
+                label.config(text='Tie!')
 
 
 def spaceIsFree(pos):
     pass
 
 
-def isWinner(bo, le):
+def isWinner():
     pass
 
 
@@ -22,15 +35,11 @@ def selectRandom(li):
     pass
 
 
-def compMove():
+def computerMove():
     pass
 
 
 def isBoardFull(board):
-    pass
-
-
-def printBoard():
     pass
 
 
@@ -42,11 +51,15 @@ def main():
     pass
 
 
+# Tkinter
 window = tk.Tk()
 window.title('Tic Tac Toe')
+
 players = ['X', 'O']
 player = random.choice(players)
-buttons = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+board = [
+    [0 for _ in range(3)] for _ in range(3)
+]  # output [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 label = tk.Label(text=player + ' Turn')
 label.pack(side='top')
@@ -57,9 +70,15 @@ restart_btn.pack(side='top')
 frame = tk.Frame(window)
 frame.pack(side='top')
 
+# Create grid
 for row in range(3):
     for column in range(3):
-        buttons[row][column] = tk.Button(frame, width=10, height=5, command='compMove')
-        buttons[row][column].grid(row=row, column=column)
+        board[row][column] = tk.Button(
+            frame,
+            width=10,
+            height=5,
+            command=lambda row=row, col=column: insertLetter(row, col),
+        )
+        board[row][column].grid(row=row, column=column)
 
 window.mainloop()
